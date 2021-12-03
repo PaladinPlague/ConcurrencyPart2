af11cc3330c2da45f5b5a2228100e9b03fea57f5
Nuoxu <liantinghan@gmail.com>
2021-12-03 19:27:29 +0000

    Part A/Task 2/1 is now complete, changes can be made upon requests.

---
+++ src/DummyGroups.java
@@ -0,0 +1,33 @@
+public class DummyGroups {
+
+    public DummyGroups(){
+        ThreadGroup a = new ThreadGroup("G(A)");
+        ThreadGroup b = new ThreadGroup("G(B)");
+        ThreadGroup c = new ThreadGroup("G(C)");
+        ThreadGroup d = new ThreadGroup("G(D)");
+        ThreadGroup ae = new ThreadGroup(a,"G(A(E))");
+
+        (new Thread(a, new DummyThreads())).start();
+        (new Thread(a, new DummyThreads())).start();
+        (new Thread(a, new DummyThreads())).start();
+        (new Thread(b, new DummyThreads())).start();
+        (new Thread(b, new DummyThreads())).start();
+        (new Thread(c, new DummyThreads())).start();
+        (new Thread(c, new DummyThreads())).start();
+        (new Thread(d, new DummyThreads())).start();
+        (new Thread(ae, new DummyThreads())).start();
+    }
+
+
+}
+class DummyThreads implements Runnable{
+    @Override
+    public void run() {
+        while (true){
+            try{
+                Thread.sleep(1000);
+                for(int i = 0; i<1000000; i++);
+            }catch (InterruptedException ie){}
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,22 @@
-
 public class Main {
 
     /*
-    * Michael's first test function.
+    * Main.java
+    * Firstly created by Jackson Blair @ Nov 18, 2021 4:11pm UTC
+    *
     */
-    private static void printThread(Thread t) {
-        if (t == null) return;
-        System.out.println("\t Thread: " + t.getName( ) +
-                    "\n\t Identifier: "+t.getId()+
-                    "\n\t Is Alive: "+ t.isAlive( )+
-                    "\n\t Priority: " + t.getPriority( ) +
-                    (t.isDaemon( )?"\n\t Daemon":"") +
-                    "\n");
-    }
-
-
-
-    /*
-     * the following functions are copied from:
-     *                                   https://www.oreilly.com/library/view/java-examples-in/0596006209/ch04s03.html
-     * replace this function with our own function.
-     */
-
-    /** Display info about a thread group and its threads and groups */
-    private static void printThreadGroup(ThreadGroup g) {
-        if (g == null) return;
-        int num_threads = g.activeCount( );
-        int num_groups = g.activeGroupCount( );
-        Thread[] threads = new Thread[num_threads];
-        ThreadGroup[] groups = new ThreadGroup[num_groups];
-
-        g.enumerate(threads, false);
-        g.enumerate(groups, false);
-
-        System.out.println("Thread Group: " + g.getName());
-
-        for(int i = 0; i < num_threads; i++)
-            printThread(threads[i]);
-        for(int i = 0; i < num_groups; i++)
-            printThreadGroup(groups[i]);
-    }
-
-
-
-    /** Find the root thread group and list it recursively */
-    public static void listAllThreads() {
-        ThreadGroup current_thread_group;
-        ThreadGroup root_thread_group;
-        ThreadGroup parent;
-
-        // Get the current thread group
-        current_thread_group = Thread.currentThread( ).getThreadGroup( );
-
-        // Now go find the root thread group
-        root_thread_group = current_thread_group;
-        parent = root_thread_group.getParent( );
-        while(parent != null) {
-            root_thread_group = parent;
-            parent = parent.getParent( );
-        }
-
-        // And list it, recursively
-        printThreadGroup(root_thread_group);
-    }
 
 
     public static void main(String[  ] args) {
-        listAllThreads();
+
+        //Building multiply dummy Thread Groups each fills with some dummy threads
+        //just check if the JVM Monitor Namely TM can pick it up.
+        DummyGroups dg = new DummyGroups();
+
+        //Create an instance of ThreadMonitor
+        ThreadMonitor TM = new ThreadMonitor();
+        //Run the Thread Monitor TM
+        TM.run();
     }
 
 
