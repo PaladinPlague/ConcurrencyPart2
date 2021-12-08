7845996a2cede4cd26b6d983c7a06d9715c9480d
Scott Love CSM2019 <scott.love.2019@uni.strath.ac.uk>
2021-12-08 16:43:38 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
 
     /*
-    * Main.java
-    * Firstly created by Jackson Blair @ Nov 18, 2021 4:11pm UTC
-    *
-    */
+     * Main.java
+     * Firstly created by Jackson Blair @ Nov 18, 2021 4:11pm UTC
+     *
+     */
 
-
-    public static void main(String[  ] args) {
+    public static void main(String[  ] args) throws InterruptedException {
 
         //Building multiply dummy Thread Groups each fills with some dummy threads
         //just check if the JVM Monitor Namely TM can pick it up.
@@ -16,8 +15,14 @@ public class Main {
         //Create an instance of ThreadMonitor
         ThreadMonitor TM = new ThreadMonitor();
         //Run the Thread Monitor TM
+        System.out.println("------------------------------------STARTING----------------------------------------");
+
         TM.run();
+        while(true) {
+            Thread.sleep(10000);
+            System.out.println("-------------------------------REFRESHING-------------------------------------------");
+            TM.run();
+
+        }
     }
-
-
 }
