e5187bc9434844100f1d30038c4f9405b56a49f8
Suleman <sulemanakhter2001@gmail.com>
2021-12-09 20:39:15 +0000

    design

--- src/Main.java
+++ src/Main.java
@@ -25,15 +25,18 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel1 = new JPanel();
         JPanel panel2 = new JPanel();
+        JPanel startPanel = new JPanel();
+        JPanel infoPanel = new JPanel();
 
 
-        frame.setSize(1920, 1080);
+        frame.setSize(1800, 400);
+        frame.setResizable(false);
 
 
 
         JButton startBtn = new JButton("Start Thread");
 
-        JTextField console = new JTextField("Check Console for output! :)");
+
 
 
 
@@ -48,15 +51,24 @@ public class Main {
         JButton searchSubmitButton = new JButton("Submit");
         JTextArea searchReturn = new JTextArea(10,10);
         searchReturn.setEditable(false);
-
         JLabel stopLabel = new JLabel("Stop Thread/s: ");
-        JTextArea stopInformation = new JTextArea("To stop a Thread, you need to enter the name of the Thread and check console for result.\n To stop multiple Threads, you need to enter the list of thread names separated with comma with no space in between.",10,10);
         JTextField stopText = new JTextField(20);
         JButton submitButton3 = new JButton("Submit");
 
+        //INFORMATION SIDE OF THINGS
 
-        panel1.add(startBtn);
-        panel1.add(console);
+        JLabel info = new JLabel("<html><body><h2>INFORMATION:</h2><p> - To start please press Start Thread, this will allow you see the thread information inside the java console</p> <p> - To stop a Thread, you need to enter the name of the Thread and check console for result</p>  <p> - To stop multiple Threads, you need to enter the list of thread names separated with comma with no space in between.</p> </body></html>");
+
+        infoPanel.add(info);
+
+        startPanel.add(startBtn);
+
+
+
+        panel2.add(stopLabel);
+        panel2.add(stopText);
+        panel2.add(submitButton3);
+
 
         panel2.add(filterLabel);
         panel2.add(filter);
@@ -71,17 +83,14 @@ public class Main {
         searchReturn.setText("Results will be displayed here!");
 
 
-        panel1.add(stopLabel);
-        panel1.add(stopText);
-        panel1.add(submitButton3);
-        panel1.add(stopInformation);
 
 
 
 
-
-        frame.getContentPane().add(panel1,"Center");
-        frame.getContentPane().add(panel2,"North");
+        frame.getContentPane().add(startPanel,BorderLayout.CENTER);
+        frame.getContentPane().add(infoPanel,BorderLayout.NORTH);
+        frame.getContentPane().add(panel1,BorderLayout.PAGE_END);
+        frame.getContentPane().add(panel2,BorderLayout.PAGE_END);
 
 
         frame.setVisible(true);
