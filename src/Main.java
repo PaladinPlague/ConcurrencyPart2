7a6dd39a90b441216893af4701f754cb1c11a825
Nuoxu <liantinghan@gmail.com>
2021-12-09 17:46:27 +0000

    my version of GUI

--- src/Main.java
+++ src/Main.java
@@ -2,84 +2,104 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.EventObject;
+import java.util.TimerTask;
 
 public class Main {
 
     /*
      * Main.java
      * Firstly created by Jackson Blair @ Nov 18, 2021 4:11pm UTC
-     *
      */
 
     public static void main(String[] args){
+
+        //Building multiply dummy Thread Groups each fills with some dummy threads
+        //just check if the JVM Monitor Namely TM can pick it up.
+        DummyGroups dg = new DummyGroups();
+
+        ThreadMonitor monitor = new ThreadMonitor();
 
         JFrame frame = new JFrame("Concurrency GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel1 = new JPanel();
         JPanel panel2 = new JPanel();
-        JPanel panel3 = new JPanel();
-        frame.setSize(750, 300);
+
+
+        frame.setSize(1080, 540);
+
+        JButton startBtn = new JButton("Start Thread");
+
+        JTextField console = new JTextField("Check Console for output! :)");
+
+
+
         JLabel filterLabel = new JLabel("Filter: ");
-        JButton button1 = new JButton("Start Thread");
         JTextField filter = new JTextField(20);
-        JButton submitButton1 = new JButton("Submit");
+        JButton filterSubmitButton = new JButton("Submit");
+        JTextArea filterReturn = new JTextArea(10,10);
+        filterReturn.setEditable(false);
+
         JLabel searchLabel = new JLabel("Search: ");
         JTextField search = new JTextField(20);
-        JButton submitButton2 = new JButton("Submit");
-        JTextField console = new JTextField(20);
+        JButton searchSubmitButton = new JButton("Submit");
+        JTextArea searchReturn = new JTextArea(10,10);
+        searchReturn.setEditable(false);
 
+        panel1.add(startBtn);
+        panel1.add(console);
 
-        panel1.add(button1);
         panel2.add(filterLabel);
         panel2.add(filter);
-        panel2.add(submitButton1);
+        panel2.add(filterSubmitButton);
+        panel2.add(filterReturn);
+        filterReturn.setText("Results will be displayed here!");
+
         panel2.add(searchLabel);
         panel2.add(search);
-        panel2.add(submitButton2);
+        panel2.add(searchSubmitButton);
+        panel2.add(searchReturn);
+        searchReturn.setText("Results will be displayed here!");
 
 
-        frame.getContentPane().add(BorderLayout.SOUTH, panel1);
-        frame.getContentPane().add(BorderLayout.BEFORE_LINE_BEGINS, panel2);
+        frame.getContentPane().add(panel1,"Center");
+        frame.getContentPane().add(panel2,"North");
+
+
         frame.setVisible(true);
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Main starting = new Main();
-                starting.start();
-            }
 
-        });
-        filter.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Helle4teteo");
-            }
+        startBtn.addActionListener(e -> {
+            startRefreshing();
         });
 
 
 
+        filterSubmitButton.addActionListener(e -> {
+            if(filter.getText()!=null){
+                filterReturn.setText(monitor.filterGroup(filter.getText()));
 
+            }
 
+        });
+
+        searchSubmitButton.addActionListener(e -> {
+            if(search.getText()!=null){
+                searchReturn.setText(monitor.searchThread(search.getText()));
+            }
+        });
 
     }
 
-    public void start(){
-        //Building multiply dummy Thread Groups each fills with some dummy threads
-        //just check if the JVM Monitor Namely TM can pick it up.
-        DummyGroups dg = new DummyGroups();
-
-        startRefreshing();
-        startFunctions();
-    }
     public static void startRefreshing(){
         Runnable refreshThread = new refreshThread();
         (new Thread(refreshThread)).start();
     }
 
-    public static void startFunctions(){
-        Runnable functionThread = new functionThread();
-        (new Thread(functionThread)).start();
-    }
+
+
+
+
+
 
 
 }
