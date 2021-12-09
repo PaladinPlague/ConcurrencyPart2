fd006ac9d66524869bae76868a523ea712b25534
Nuoxu Li CSM2019 <nuoxu.li.2019@uni.strath.ac.uk>
2021-12-09 14:26:49 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,53 @@ public class Main {
 
     public static void main(String[  ] args){
 
+        JFrame frame = new JFrame("Concurrency GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        frame.setSize(750, 300);
+        JLabel filterLabel = new JLabel("Filter: ");
+        JButton button1 = new JButton("Start Thread");
+        JTextField filter = new JTextField(20);
+        JButton submitButton1 = new JButton("Submit");
+        JLabel searchLabel = new JLabel("Search: ");
+        JTextField search = new JTextField(20);
+        JButton submitButton2 = new JButton("Submit");
+        JTextField console = new JTextField(20);
+
+
+        panel1.add(button1);
+        panel2.add(filterLabel);
+        panel2.add(filter);
+        panel2.add(submitButton1);
+        panel2.add(searchLabel);
+        panel2.add(search);
+        panel2.add(submitButton2);
+
+
+        frame.getContentPane().add(BorderLayout.SOUTH, panel1);
+        frame.getContentPane().add(BorderLayout.BEFORE_LINE_BEGINS, panel2);
+        frame.setVisible(true);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main starting = new Main();
+                try {
+                    starting.start();
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+            }
+
+        });
+        filter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Helle4teteo");
+            }
+        });
+
         //Building multiply dummy Thread Groups each fills with some dummy threads
         //just check if the JVM Monitor Namely TM can pick it up.
         DummyGroups dg = new DummyGroups();
